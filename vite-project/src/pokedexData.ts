b861b23ef0f4fae4d8f9208f44006049fc190f0a
joaquim <joaquim.salviano@estudante.ufjf.br>
2025-11-06 20:29:13 -0300

    adicionando mais pokemons

--- vite-project/src/pokedexData.ts
+++ vite-project/src/pokedexData.ts
@@ -52,7 +52,7 @@ export const todosPokemons: Pokemon[] = [
     vantagens: ['Água', 'Voador'],
   },
   {
-    id: 7, nome: 'Squirtle', numero: '#007', selecionado: false,
+    id: 7, nome: 'Squirtle', numero: '#007', selecionado: true,
     imagemUrl: 'https://img.pokemondb.net/sprites/black-white/anim/normal/squirtle.gif',
     tipos: ['Água'],
     estatisticas: [ 
@@ -63,7 +63,7 @@ export const todosPokemons: Pokemon[] = [
     vantagens: ['Fogo', 'Terra', 'Pedra'],
   },
   {
-    id: 94, nome: 'Gengar', numero: '#094', selecionado: false,
+    id: 94, nome: 'Gengar', numero: '#094', selecionado: true,
     imagemUrl: 'https://img.pokemondb.net/sprites/black-white/anim/normal/gengar.gif',
     tipos: ['Fantasma', 'Venenoso'],
     estatisticas: [
@@ -112,7 +112,7 @@ export const todosPokemons: Pokemon[] = [
 
   // Dragonite (modelo original)
   {
-    id: 149, nome: 'Dragonite', numero: '#149', selecionado: false,
+    id: 149, nome: 'Dragonite', numero: '#149', selecionado: true,
     imagemUrl: 'https://img.pokemondb.net/sprites/black-white/anim/normal/dragonite.gif',
     tipos: ['Dragão', 'Voador'],
     estatisticas: [
@@ -161,7 +161,7 @@ export const todosPokemons: Pokemon[] = [
 
   // Jigglypuff (modelo original)
   {
-    id: 39, nome: 'Jigglypuff', numero: '#039', selecionado: false,
+    id: 39, nome: 'Jigglypuff', numero: '#039', selecionado: true,
     imagemUrl: 'https://img.pokemondb.net/sprites/black-white/anim/normal/jigglypuff.gif',
     tipos: ['Normal', 'Fada'],
     estatisticas: [
@@ -208,7 +208,7 @@ export const todosPokemons: Pokemon[] = [
 
   // Gyarados (modelo original)
   {
-    id: 130, nome: 'Gyarados', numero: '#130', selecionado: false,
+    id: 130, nome: 'Gyarados', numero: '#130', selecionado: true,
     imagemUrl: 'https://img.pokemondb.net/sprites/black-white/anim/normal/gyarados.gif',
     tipos: ['Água', 'Voador'],
     estatisticas: [
@@ -288,7 +288,7 @@ export const todosPokemons: Pokemon[] = [
     vantagens: ['Fogo', 'Terra', 'Pedra'],
   },
   {
-    id: 8, nome: 'Wartortle', numero: '#008', selecionado: false,
+    id: 8, nome: 'Wartortle', numero: '#008', selecionado: true,
     imagemUrl: 'https://img.pokemondb.net/sprites/black-white/anim/normal/wartortle.gif',
     tipos: ['Água'],
     estatisticas: [
@@ -330,5 +330,38 @@ export const todosPokemons: Pokemon[] = [
     ],
     fraquezas: ['Elétrico', 'Grama'],
     vantagens: ['Fogo', 'Terra', 'Pedra'],
+  },
+  {
+    id: 344, nome: 'Clamperl', numero: '#344', selecionado: true,
+    imagemUrl: 'https://img.pokemondb.net/sprites/black-white/anim/normal/clamperl.gif',
+    tipos: ['Água'],
+    estatisticas: [
+      { nome: 'HP', valor: 35 }, { nome: 'ATK', valor: 64 }, { nome: 'DEF', valor: 85 },
+      { nome: 'Sp.ATK', valor: 74 }, { nome: 'Sp.DEF', valor: 55 }, { nome: 'SPEED', valor: 32 },
+    ],
+    fraquezas: ['Elétrico', 'Grama'],
+    vantagens: ['Fogo', 'Terra', 'Pedra'],
+  },
+  {
+    id: 176, nome: 'Togetic', numero: '#176', selecionado: true,
+    imagemUrl: 'https://img.pokemondb.net/sprites/black-white/anim/normal/togetic.gif',
+    tipos: ['Fada', 'Voador'],
+    estatisticas: [
+      { nome: 'HP', valor: 55 }, { nome: 'ATK', valor: 40 }, { nome: 'DEF', valor: 85 },
+      { nome: 'Sp.ATK', valor: 80 }, { nome: 'Sp.DEF', valor: 105 }, { nome: 'SPEED', valor: 40 },
+    ],
+    fraquezas: ['Elétrico', 'Gelo', 'Pedra'],
+    vantagens: ['Lutador', 'Inseto', 'Fada'],
+  },
+  {
+    id: 500, nome: 'Emolga', numero: '#500', selecionado: true,
+    imagemUrl: 'https://img.pokemondb.net/sprites/black-white/anim/normal/emolga.gif',
+    tipos: ['Elétrico', 'Voador'],
+    estatisticas: [
+      { nome: 'HP', valor: 55 }, { nome: 'ATK', valor: 75 }, { nome: 'DEF', valor: 60 },
+      { nome: 'Sp.ATK', valor: 75 }, { nome: 'Sp.DEF', valor: 60 }, { nome: 'SPEED', valor: 103 },
+    ],
+    fraquezas: ['Gelo', 'Terra', 'Pedra'],
+    vantagens: ['Água', 'Voador', 'Lutador'],
   },
 ];
